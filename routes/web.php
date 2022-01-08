e5a2a50c99c9ef41b0f54fa597c87975d5fef799
Siarhei Volkau <musicy@yandex.ru>
2022-01-08 22:32:03 +0100

    add AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin', 'namespace' => 'App\Http\Controllers\Admin'], function () {
+    Route::get('/', 'MainController@index')->name('admin.index');
+});
