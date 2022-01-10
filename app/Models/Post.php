e7a71cd797ac14315a0b718c31e1b0362a559bd1
Siarhei Volkau <musicy@yandex.ru>
2022-01-10 22:03:06 +0100

    Add Laravel Sluggable tools

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
 
 class Post extends Model
 {
+    use HasSlug;
     use HasFactory;
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Tag extends Model
 {
+    use HasSlug;
+
     use HasFactory;
+    public function posts(){
+        return $this->belongsToMany(Post::class);
+    }
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
